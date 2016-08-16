3c688c0131960280f6d3112f4794ea563cb5b37d
Anand Angalig and Kyle Fisher <kyle@>
2016-08-16 09:17:26 -0400

    create the html skeleton

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  $("form#sorter").submit(function(event) {
+    event.preventDefault();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // var groceryList = ["word1", "word2", "word3", "word4", "word5", "word6"];
+    // var organizedList = groceryList.map(function(groceryItem) {
+    //   var newWords = $("#" + groceryItem).val();
+    //   return newWords;
+    // });
+    // var sortDisplay = function() {
+    //   $("form#sorter").hide();
+    //   $("sorted-list").html("");
+    //   organizedList.sort().forEach(function(item) {
+    //     $("#sorted-list").append("<li>" + item + "</li>");
+    //   });
+    // }
+    // sortDisplay();
